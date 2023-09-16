b7e735e510853f6c4cab922fb9fe5efce593401a
AdeyBoy123 <groveraden@gmail.com>
2023-09-16 12:56:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doubleChar
+{
+    internal class Program
+    {
+        static string DoubleChar(string input)
+        {
+            char[] doubledChars = new char[input.Length * 2];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                doubledChars[i*2] = input[i];
+                doubledChars[i*2+1] = input[i];
+            }
+
+            return new string(doubledChars);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string of your choice: ");
+            string input = Console.ReadLine();
+
+            string output = DoubleChar(input);
+
+            Console.WriteLine("Result: " + output);
+
+        }
+    }
+}
